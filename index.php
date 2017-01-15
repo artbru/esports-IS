a2100aac3e6ef540478098278a550d8683e9d63c
Edvinas Steponavičius <steponaviciusedvinas@gmail.com>
2017-01-15 14:49:45 +0200

    UI improvements

--- index.php
+++ index.php
@@ -46,50 +46,97 @@
 		<meta name="robots" content="noindex">
 		<title>Sporto Organizacijų IS</title>
 		<link rel="stylesheet" type="text/css" href="scripts/datetimepicker/jquery.datetimepicker.css" media="screen" />
-		<link rel="stylesheet" type="text/css" href="style/main.css" media="screen" />
-		<script type="text/javascript" src="scripts/jquery-1.12.0.min.js"></script>
+        <script type="text/javascript" src="scripts/jquery-1.12.0.min.js"></script>
 		<script type="text/javascript" src="scripts/datetimepicker/jquery.datetimepicker.full.min.js"></script>
 		<script type="text/javascript" src="scripts/main.js"></script>
-	</head>
-	<body>
-		<div id="body">
-			<div id="header">
-				<h3 id="slogan"><a href="index.php">Sporto Organizacijų IS</a></h3>
-			</div>
-			<div id="content">
-				<div id="topMenu">
-					<ul class="float-left">
-						<li><a href="index.php?module=country" title="Šalys"<?php if($module == 'countries') { echo 'class="active"'; } ?>>Šalis</a></li>
-						<li><a href="index.php?module=tournament" title="Turnyrai"<?php if($module == 'service') { echo 'class="active"'; } ?>>Turnyras</a></li>
-						<li><a href="index.php?module=referee" title="Arbitrai"<?php if($module == 'customer') { echo 'class="active"'; } ?>>Arbitras</a></li>
-						<li><a href="index.php?module=player" title="Žaidėjai"<?php if($module == 'employee') { echo 'class="active"'; } ?>>Žaidėjas</a></li>
-						<li><a href="index.php?module=coach" title="Treneriai"<?php if($module == 'car') { echo 'class="active"'; } ?>>Treneris</a></li>
-						<li><a href="index.php?module=sponsor" title="Remėjai"<?php if($module == 'brand') { echo 'class="active"'; } ?>>Remėjas</a></li>
-						<li><a href="index.php?module=team" title="Komandos"<?php if($module == 'tean') { echo 'class="active"'; } ?>>Komandos</a></li>
-						<li><a href="index.php?module=arena" title="Arenos"<?php if($module == 'arena') { echo 'class="active"'; } ?>>Arenos</a></li>
-						<li><a href="index.php?module=agent" title="Agentai"<?php if($module == 'agent') { echo 'class="active"'; } ?>>Agentai</a></li>
-						<li><a href="index.php?module=city" title="Miestai"<?php if($module == 'city') { echo 'class="active"'; } ?>>Miestai</a></li>
-					</ul>
-					<ul class="float-right">
-						<li><a href="index.php?module=report" title="Ataskaitos"<?php if($module == 'report') { echo 'class="active"'; } ?>>Ataskaitos</a></li>
-					</ul>
-				</div>
-				<div id="contentMain">
-					<?php
-						if(!empty($module)) {
-							if(empty($id) && empty($action)) {
-								include "controls/{$module}_list.php";
-							} else {
-								include "controls/{$module}_edit.php";
-							}
-						}
-					?>
-					<div class="float-clear"></div>
-				</div>
-			</div>
-			<div id="footer">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-			</div>
-		</div>
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+        <link rel="stylesheet" type="text/css" href="style/main.css" media="screen" />
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    </head>
+	<body>
+
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Brand</a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                        <li><a href="#">Link</a></li>
+                    </ul>
+                    <form class="navbar-form navbar-right">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Username">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Password">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Login</button>
+                        <button type="button" class="btn btn-info">Register</button>
+                        <div class="row">
+                            <!--<div class="form-group">
+                                <div class="checkbox col-md-8">
+                                    <label>
+                                        <input type="checkbox" name="remember"> Prisiminti mane
+                                    </label>
+                                </div>
+                            </div>-->
+                            <div class="col-md-4 col-md-offset-8">
+                                <a href="#">Forgot password?</a>
+                            </div>
+                        </div>
+                    </form>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+
+        <div class="container main">
+            <div class="container col-md-2">
+                <div class="row well">
+                    <p>Test text</p>
+                </div>
+                <div class="row well">
+                    <p>Test text</p>
+                </div>
+            </div>
+
+            <div class="container col-md-10 well">
+                <p>Test text</p>
+                <?php
+                if(!empty($module)) {
+                    if(empty($id) && empty($action)) {
+                        include "controls/{$module}_list.php";
+                    } else {
+                        include "controls/{$module}_edit.php";
+                    }
+                }
+                ?>
+                <!--<div class="float-clear"></div>-->
+            </div>
+        </div>
+
+        <!--
+        <div id="footer">
+
+        </div>
+        -->
 	</body>
 </html>
