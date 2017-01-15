b22df6ea0975964112687cba52bf0e0265c7d233
artbru <bruart@inbox.lt>
2017-01-15 21:08:35 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 class config {
 	const DB_SERVER    = 'localhost';
-	const DB_NAME      = 'so galutinis';
+	const DB_NAME      = 'esports';
 	const DB_USERNAME  = 'root';
 	const DB_PASSWORD  = '';
 }
