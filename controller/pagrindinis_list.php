e8997bf8f76ee8dc213b2c3d7f8f5208fc89f1f4
artbru <bruart@inbox.lt>
2017-01-16 17:37:09 +0200

    Add files via upload

---
+++ controller/pagrindinis_list.php
@@ -0,0 +1,37 @@
+<p class=MsoNormal style='text-align:justify;text-indent:.25in'><span
+style='font-size:26.0pt;line-height:107%'>eSports</span></p>
+
+<p class=MsoNormal style='text-align:justify;text-indent:.25in'>&nbsp;</p>
+
+<p class=MsoNormal style='text-align:justify;text-indent:.25in'>Komand&#371;
+valdymo posistem&#279;. Posistem&#279; skirta registruoti, redaguoti ir šalinti
+komandas. Taip pat posistem&#279;je vykdoma komandai priklausan&#269;i&#371;
+nari&#371; registracija, j&#371; informacijos redagavimas, nari&#371;
+pašalinimas iš komandos. Registruojant nauj&#261; komand&#261; nurodomas jos
+pavadinimas, regionas kuriam komanda atstovauja, žaidžiamas žaidimas, reitingas
+tarp komand&#371; žaidžian&#269;i&#371; tame pa&#269;iame regione bei reitingas
+tarp komand&#371; pasaulyje, komandos internet svetain&#279;, nari&#371;
+(žaid&#279;j&#371;) atstovaujan&#269;i&#371; komandai skai&#269;ius.
+Registruojan žaid&#279;j&#261; atstovaujant&#303; komandai yra nurodomas jo
+vardas, pavard&#279;, naudojamas vardas žaidime, žaid&#279;jo gimimo data bei
+tautyb&#279;. </p>
+
+<p class=MsoNormal style='text-align:justify;text-indent:.25in'><span lang=LT>Turnyr&#371;
+administravimo posistem&#279;. Posistem&#279; skirta registruoti, redaguoti ir
+šalinti turnyrus. Turnyrus gali administruoti Organizatoriai. Kiekvienas
+turnyras turi unikal&#371; identifikacijos kod&#261;, pavadinim&#261;, žaidimo
+pavadinim&#261;, prizin&#303; fond&#261;, pražios ir pabaigos dat&#261;,
+dalyvaujan&#269;ias komandas(8 komandos), miest&#261;, šalies pavadinim&#261;.
+Jei turnyras jau pasibaig&#281;s, organizatorius nurodo turnyro
+nugal&#279;toj&#261;. Registruojant turnyr&#261;, pasirenkama sporto šaka
+(žaidimas), užpildomi duomenys, ir iš galim&#371; komand&#371; s&#261;rašo
+parenkamos dalyvaujan&#269;ios komandos. Komandos negali dalyvauti keliuose
+turnyruose, kurie vyksta vienu metu.  Šioje posistem&#279;je taip pat galima
+gauti turnyr&#371; ataskaitas, bei šali&#371; turnyr&#371; ataskaitas.
+Turnyr&#371; ataskaitoje pateikiami pasirinktos eSporto šakos turnyrai
+(pavadinimas, vieta, data, prizinis fondas, nugal&#279;tojas). Šali&#371; turnyr&#371;
+ataskaitoje pateikiamas šali&#371; s&#261;rašas ir informacija kiek šalyje vyko
+turnyr&#371; nurodyto laiko tarpe. Ataskaitos pateikiama informacija – šalis,
+turnyr&#371; skai&#269;ius, bendras prizinis fondas.</span></p>
+
+<p class=MsoNormal>&nbsp;</p>
